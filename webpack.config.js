bcfd67cfe9391550b16ea624f9092c71dcc4829c
yadong <floiges@163.com>
2019-03-08 17:29:49 +0800

    webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,30 @@
 const path = require('path');
+/** 
+ * 虽然在 dist/ 文件夹我们已经有 index.html 这个文件，
+ * 然而 HtmlWebpackPlugin 还是会默认生成 index.html 文件。
+ * 这就是说，它会用新生成的 index.html 文件，把我们的原来的替换
+*/
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 自动添加 bundle 到 html 中
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // 在每次构建前清理 /dist 文件夹
 
+// current src-output
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src-output/index.js',
+        print: './src-output/print.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
+        /**
+         * With zero configuration,
+         *   clean-webpack-plugin will remove files inside the directory below
+         */
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            }
-        ]
-    }
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ]
 };
